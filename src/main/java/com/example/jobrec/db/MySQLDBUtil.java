78436c4aca9f1129dcd8f86c2eefce5a6dee9159
lospoloes <108310531+lospoloes@users.noreply.github.com>
2025-04-04 22:57:49 +0800

    Update MySQLDBUtil.java

--- src/main/java/com/example/jobrec/db/MySQLDBUtil.java
+++ src/main/java/com/example/jobrec/db/MySQLDBUtil.java
@@ -5,7 +5,7 @@ public class MySQLDBUtil {
     private static final String PORT_NUM = "3306";
     public static final String DB_NAME = "jobrecdb";
     private static final String USERNAME = "admin";
-    private static final String PASSWORD = "Lyc10086!";
+    private static final String PASSWORD = "****";
     public static final String URL = "jdbc:mysql://"
             + INSTANCE + ":" + PORT_NUM + "/" + DB_NAME
             + "?user=" + USERNAME + "&password=" + PASSWORD
